bbf8b9e256706a89b19796523370fb2ec6578e64
Filipe <filipe.queiroz.abreu@gmail.com>
2024-08-30 13:13:48 -0300

    updates

---
+++ PowerShell studying/Get-Service.ps1
@@ -0,0 +1,7 @@
+﻿#Get service
+
+#$_ is reverencing the result from "Get-Service" comman, the .Status a property from the result
+#Get-Service has 3 properties, Status Name and Display name, we could do the same for each one of them
+Get-Service | Where-object {$_.Status -eq "Running"} #Filters only the running services
+
+Get-Service | Where-object {$_.Status -eq "Stopped"} #Filters the stopped services
---
+++ PowerShell studying/String_Manipulations.ps1
@@ -0,0 +1,29 @@
+﻿#Replace
+
+#String Manipulations
+
+function replace_example {
+
+Write-Host This function is running the following command
+Write-Host ´Filipe Queiroz" -Replace "Queiroz", "Nunes´#Replaces the "Queiroz" part with "Nunes"
+"Filipe Queiroz" -Replace "Queiroz", "Nunes" 
+}
+
+function select_string_example {
+
+Write-Host This function is running the following command
+Write-Host Tipconfig | Select-String "IPv4" #Search for only the strings with "IPv4"
+ipconfig | Select-String "IPv4"
+}
+
+function select_string_plus_replace {
+Write-Host This function is running the following command
+Write-Host "ipconfig | Select-String "IPv4" | ForEach-Object {$_ -Replace '.*\s',''} " 
+Write-Host ""
+ipconfig | Select-String "IPv4" | ForEach-Object {$_ -Replace '.*\s',''} #It iterates over each matching line and removes character based on this REGEX expressio
+# For the regex, the expression '.*\s' means ANY CHARACTER (.) fowolled by an WHITE SPACE (\n)it removes any charecter
+So than we can return only the IPV4 address
+}
+
+
+select_string_plus_replace
