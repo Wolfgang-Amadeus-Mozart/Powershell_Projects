4f069aa2f718a910e2c5d92f95ce5c72d0e08115
Filipe <filipe.queiroz.abreu@gmail.com>
2024-09-10 09:22:37 -0300

    set_alias

---
+++ PowerShell studying/Set_Alias.ps1
@@ -0,0 +1,31 @@
+﻿# Set_Alias - This script shows how to create an Alias
+
+#
+
+# Site:       https://filipedeabreu.com
+
+# Author:      Filipe de Abreu
+
+# Maintenance:  Filipe de Abreu
+
+# Note: This script has been sanitized to remove company-specific details.
+
+# ------------------------------------------------------------------------ #
+# Tested in:
+# OS: Windows 11
+#Name                           Value                                                                                                                                                          
+#----                           -----                                                                                                                                                          
+# PSVersion                      5.1.22621.3958                                                                                                                                                 
+# PSEdition                      Desktop  
+
+#function that creates the alias
+function create_alias {
+    param($aliasName, $command)
+
+    
+    Set-Alias $aliasName $command
+
+    echo "You've set the Alias Name $aliasName to the command $command"
+}
+
+create_alias -aliasName EName -command #<your command here>
---
+++ PowerShell studying/formatting.ps1
@@ -0,0 +1,43 @@
+﻿# Formatting - This script shows a simple way of formatting outputs in powershell
+
+#
+
+# Site:       https://filipedeabreu.com
+
+# Author:      Filipe de Abreu
+
+# Maintenance:  Filipe de Abreu
+
+# Note: This script has been sanitized to remove company-specific details.
+
+# ------------------------------------------------------------------------ #
+# Tested in:
+# OS: Windows 11
+#Name                           Value                                                                                                                                                          
+#----                           -----                                                                                                                                                          
+# PSVersion                      5.1.22621.3958                                                                                                                                                 
+# PSEdition                      Desktop  
+
+
+function format-wide-func {
+    param($cmdLet) #Gets a cmdLet, like (get-process)
+    return $cmdLet | Format-Wide #Print it in format-wide 
+}
+
+function format-wide-with-propertie-select{
+    param($function) #Get the return of the "format-wide-func" function
+    $arrayOfProperties = @("$null") 
+    $properties = $var | get-member | where membertype -eq "AliasProperty" #filters the properties
+
+    #Concat an array with each property from the cmdlet
+    foreach ($propertie in $properties.name){
+        $arrayOfProperties += $propertie
+        }
+
+    echo "Please select the property from the cmdlet you want to look"
+
+    
+
+}
+
+format-wide-func -cmdLet (Get-Process)
